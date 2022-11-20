2657f0746d54364dba4faf8ba6abe378b0e66b31
Sergio Salinas <salinas@cs.wichita.edu>
2022-11-20 11:41:28 -0600

    fix typos

--- build-cnn-model.py
+++ build-cnn-model.py
@@ -20,9 +20,6 @@ argument `input_shape` to your first layer.
 model =   tf.keras.models.Sequential()
 model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-# model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
-# model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-# model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
 
 
 """
